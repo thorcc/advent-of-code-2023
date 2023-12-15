9508399bbeaa3f542a837652a505c8bcbd91ad9e
thorcc <thorchrcow@gmail.com>
2023-12-15 12:51:09 +0100

    dag 10b - forsøk 1

--- 10/10.py
+++ 10/10.py
@@ -1,5 +1,5 @@
 # from collection import defaultdict
-file = open("input.txt")
+file = open("example.txt")
 data = file.read()
 lines = data.split("\n")
 
@@ -50,8 +50,93 @@ while len(queue) > 0:
             visited[n] = visited[current] + 1
             queue.append(n)
 
-current = start
-neighbors = find_neighbors(current)
 distances = list(visited.values())
 distances.sort()
 print(distances[-1])
+
+visited = {}
+visited[start] = [start]
+queue = [start]
+
+while len(queue) > 0:
+    current = queue.pop(0)
+    neighbors = find_neighbors(current)
+    for n in neighbors:
+        if n not in visited:
+            visited[n] = visited[current] + [n]
+            queue.append(n)
+
+
+loop = list(visited.values())[-1] + list(visited.values())[-2]
+loop.sort()
+# print(loop)
+
+size_x = len(lines)
+size_y = len(lines[0])
+
+def check_up(i, j):
+    k = i
+    while k > -1:
+        if (k,j) in loop:
+            return True
+        k -= 1
+    return False
+
+def check_down(i, j):
+    k = i
+    while k < size_x:
+        if (k,j) in loop:
+            return True
+        k += 1
+    return False
+
+def check_left(i, j):
+    k = j
+    while k > -1:
+        if (i,k) in loop:
+            return True
+        k -= 1
+    return False
+
+def check_right(i, j):
+    k = j
+    while k < size_y:
+        if (i,k) in loop:
+            return True
+        k += 1
+    return False
+
+
+def check_inside(i, j):
+    return check_up(i, j) and check_down(i, j) and check_left(i, j) and check_right(i, j)
+
+inside = []
+for i in range(size_x):
+    for j in range(size_y):
+        if (i,j) not in loop:
+            if check_inside(i,j):
+                inside.append((i,j))
+print(inside)
+
+
+
+import matplotlib.pyplot as plt
+plt.scatter(*zip(*loop))
+# plt.show()
+plt.scatter(*zip(*inside))
+plt.show()
+
+
+# visited = {}
+
+# def dfs(current, i):
+#     visited[current] = i
+#     neighbors = find_neighbors(current)
+#     for n in neighbors:
+#         if n not in visited or visited[n] > i:
+#             dfs(n, i + 1)
+
+# dfs(start, 0)
+# distances = list(visited.values())
+# distances.sort()
+# print(distances[-1])
